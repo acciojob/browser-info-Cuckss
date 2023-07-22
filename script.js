e46a6dc72f61ceff91021c0bf66a299de2362573
Cuckss <122050270+Cuckss@users.noreply.github.com>
2023-07-22 12:40:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let para=document.createElement("p");
+let browser=navigator.appName;
+let version=navigator.appVersion;
+para.innerHTML=" you are using "+browser+" version "+version;
+document.body.appendChild(para);
